92383bb67f74a05fae22ef10a3961eafbcda8fca
Parker Nix <71665527+ParkerNix@users.noreply.github.com>
2021-03-05 00:27:04 -0500

    Date range and name now working, next is probably modal

--- Javasheet.js
+++ Javasheet.js
@@ -15,10 +15,6 @@ function logButton(button) {
     console.log(`user pressed ` + button);
 } 
 
-function logSign(sign) {
-    console.log(`user is ` + sign);
-}
-
 hu.addEventListener('click', function () {logButton('hu')}, false);
 tbo.addEventListener('click', function () {logButton('tbo')}, false);
 yk.addEventListener('click', function () {logButton('yk')}, false);
@@ -32,28 +28,51 @@ mgt.addEventListener('click', function () {logButton('mgt')}, false);
 shh.addEventListener('click', function () {logButton('shh')}, false);
 eh.addEventListener('click', function () {logButton('eh')}, false);
 
-if ((whichMonth == 12 && whichDayOfMonth >= 22) || (whichMonth == 1 && whichDayOfMonth <= 19)) {
-    AstroSign = "Cap";
-  } else if ((whichMonth == 11 && whichDayOfMonth >= 22) || (whichMonth == 12 && whichDayOfMonth <= 21)) {
-    AstroSign = "Sag";
-  } else if ((whichMonth == 10 && whichDayOfMonth >= 24) || (whichMonth == 11 && whichDayOfMonth <= 21)) {
-    AstroSign = "Sco";
-  } else if ((whichMonth == 9 && whichDayOfMonth >= 23) || (whichMonth == 10 && whichDayOfMonth <= 23)) {
-    AstroSign = "Lib";
-  } else if ((whichMonth == 8 && whichDayOfMonth >= 23) || (whichMonth == 9 && whichDayOfMonth <= 22)) {
-    AstroSign = "Vir";
-  } else if ((whichMonth == 7 && whichDayOfMonth >= 23) || (whichMonth == 8 && whichDayOfMonth <= 22)) {
-    AstroSign = "Leo";
-  } else if ((whichMonth == 6 && whichDayOfMonth >= 22) || (whichMonth == 7 && whichDayOfMonth <= 22)) {
-    AstroSign = "Can";
-  } else if ((whichMonth == 5 && whichDayOfMonth >= 21) || (whichMonth == 6 && whichDayOfMonth <= 21)) {
-    AstroSign = "Gem";
-  } else if ((whichMonth == 4 && whichDayOfMonth >= 20) || (whichMonth == 5 && whichDayOfMonth <= 20)) {
-    AstroSign = "Tau";
-  } else if ((whichMonth == 3 && whichDayOfMonth >= 21) || (whichMonth == 4 && whichDayOfMonth <= 19)) {
-    AstroSign = "Ari";
-  } else if ((whichMonth == 2 && whichDayOfMonth >= 19) || (whichMonth == 3 && whichDayOfMonth <= 20)) {
-    AstroSign = "Pis";
-  } else if ((whichMonth == 1 && whichDayOfMonth >= 20) || (whichMonth == 2 && whichDayOfMonth <= 18)) {
-    AstroSign = "Aqu";
-  }
+const submit = document.getElementById('submit');
+
+submit.addEventListener('click', function () {console.log('User hit submit')
+
+    const fName = document.getElementById('fname').value;
+    console.log(`user's name is ` + fName);
+
+    const birthday = new Date(document.getElementById('birthday').value);
+    console.log(`user's birthday is ` + birthday);
+
+    let whichMonth = birthday.getUTCMonth();
+    whichMonth++;
+    console.log(`whichMonth is now ` + whichMonth);
+
+    const whichDayOfMonth = birthday.getUTCDate();
+    console.log(`whichDayOfMonth is now ` + whichDayOfMonth);
+
+    let sign = "";
+
+    if ((whichMonth == 12 && whichDayOfMonth >= 22) || (whichMonth == 1 && whichDayOfMonth <= 19)) {
+        sign = "mgt";
+        logSign(sign)
+    } else if ((whichMonth == 11 && whichDayOfMonth >= 22) || (whichMonth == 12 && whichDayOfMonth <= 21)) {
+        sign = "id";
+    } else if ((whichMonth == 10 && whichDayOfMonth >= 24) || (whichMonth == 11 && whichDayOfMonth <= 21)) {
+        sign = "shh";
+    } else if ((whichMonth == 9 && whichDayOfMonth >= 23) || (whichMonth == 10 && whichDayOfMonth <= 23)) {
+        sign = "ygn";
+    } else if ((whichMonth == 8 && whichDayOfMonth >= 23) || (whichMonth == 9 && whichDayOfMonth <= 22)) {
+        sign = "pft";
+    } else if ((whichMonth == 7 && whichDayOfMonth >= 23) || (whichMonth == 8 && whichDayOfMonth <= 22)) {
+        sign = "yk";
+    } else if ((whichMonth == 6 && whichDayOfMonth >= 22) || (whichMonth == 7 && whichDayOfMonth <= 22)) {
+        sign = "sm";
+    } else if ((whichMonth == 5 && whichDayOfMonth >= 21) || (whichMonth == 6 && whichDayOfMonth <= 21)) {
+        sign = "tbo";
+    } else if ((whichMonth == 4 && whichDayOfMonth >= 20) || (whichMonth == 5 && whichDayOfMonth <= 20)) {
+        sign = "cb";
+    } else if ((whichMonth == 3 && whichDayOfMonth >= 21) || (whichMonth == 4 && whichDayOfMonth <= 19)) {
+        sign = "hu";
+    } else if ((whichMonth == 2 && whichDayOfMonth >= 19) || (whichMonth == 3 && whichDayOfMonth <= 20)) {
+        sign = "eh";
+    } else if ((whichMonth == 1 && whichDayOfMonth >= 20) || (whichMonth == 2 && whichDayOfMonth <= 18)) {
+        sign = "wtf";
+    }
+
+  console.log(`sign is now ` + sign);
+});
